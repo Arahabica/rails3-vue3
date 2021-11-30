0f1ebcdec115c92e97301f3360dfbaa4542658e3
Arahabica <r.sasage@gmail.com>
2021-11-30 15:41:04 +0900

    init

---
+++ spa/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    port: 8888,
+    proxy: {
+      '^/api/': {
+        target: 'http://localhost:3000',
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }
+    }
+  }
+}
